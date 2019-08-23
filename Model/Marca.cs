03973c989196a9e737b3b2a982f10750e2d5cf6c
brenochagas85 <49415489+brenochagas85@users.noreply.github.com>
2019-08-23 16:28:00 -0300

    Criado o model de Marca

---
+++ Model/Marca.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Marca
+    {
+        [Column("nome")]
+        public string Nome { get; set; }
+
+    }
+}
