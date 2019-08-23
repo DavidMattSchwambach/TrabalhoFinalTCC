0a4daff29a9364a5e00a8704854dcdaa5b24e06c
brenochagas85 <49415489+brenochagas85@users.noreply.github.com>
2019-08-23 16:25:41 -0300

    Criado o model do cliente

---
+++ Model/Cliente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Cliente
+    {
+        [Column("nome")]
+        public string Nome { get; set; }
+
+        [Column("data_nascimento")]
+        public DateTime DataNascimento { get; set; }
+
+        [Column("cpf")]
+
+        public string Cpf { get; set; }
+
+        [Column("telefone")]
+
+        public string Telefone { get; set; }
+
+        [Column("email")]
+
+        public string Email { get; set; }
+
+        [Column("senha")]
+
+        public string Senha { get; set; }
+    }
+}
