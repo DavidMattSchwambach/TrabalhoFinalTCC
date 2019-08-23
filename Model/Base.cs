9cc28160faefd8037d7aacd5c1247502648cd519
brenochagas85 <49415489+brenochagas85@users.noreply.github.com>
2019-08-23 16:23:50 -0300

    Adicionado o conteúdo da base

--- Model/Base.cs
+++ Model/Base.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,11 @@ namespace Model
 {
     public class Base
     {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("registro_ativo")]
+        public bool RegistroAtivo { get; set; }
     }
 }
