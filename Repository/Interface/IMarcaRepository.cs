eb0e96e13fa0a1cca408069859cd93c106a5b8aa
brenochagas85 <49415489+brenochagas85@users.noreply.github.com>
2019-08-23 17:02:21 -0300

    Criado o IMarcaRepository

---
+++ Repository/Interface/IMarcaRepository.cs
@@ -0,0 +1,22 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interface
+{
+    interface IMarcaRepository
+    {
+        int Inserir(Marca marca);
+
+        bool Atualizar(Marca marca);
+
+        bool Apagar(int id);
+
+        Marca ObterPeloId(int id);
+
+        List<Marca> ObterTodos(string busca);
+    }
+}
