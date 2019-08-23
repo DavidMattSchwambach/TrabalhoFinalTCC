704fd60a5ea413fc714bb979e72655cd495b85a0
DavidMattSchwambach <davidschwambach@hotmail.com>
2019-08-23 16:05:16 -0300

    Implementado o Model Bebida e criado o Model Tipo.

--- Model/Bebidas.cs
+++ Model/Bebidas.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model
 {
+    [Table("bebidas")]
     class Bebidas
     {
+        [Column("nome")]
+        public string Tipo { get; set; }
+        [Column("valor")]
+        public string Valor { get; set; }
+
+        [ForeignKey("IdTipo")]
+        public Tipo tipo { get; set; }
+
+        [Column("id_tipo")]
+        public int IdTipo { get; set; }
+        public bool RegistroAtivo { get; set; }
     }
 }
