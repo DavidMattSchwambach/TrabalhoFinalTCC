fbd937390802adbc232f5581df2fc6d637ba26ec
DavidMattSchwambach <davidschwambach@hotmail.com>
2019-08-23 16:42:30 -0300

    Implementado o Model Tipo

--- Model/Tipo.cs
+++ Model/Tipo.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model
 {
+    [Table("tipos")]
     public class Tipo
     {
+        [Column("nome")]
+        public string Nome { get; set; }
+
+        [ForeignKey("IdMarca")]
+        public Marca Marca { get; set; }
+
+        [Column("id_marca")]
+        public int IdMarca { get; set; }
+        public bool RegistroAtivo { get; set; }
     }
 }
