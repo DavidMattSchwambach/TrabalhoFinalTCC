6f28d249b0ee3cb7a53a8dffcba86d98c42760c7
maxjulianog <maxjulianog@gmail.com>
2019-08-27 14:56:54 -0300

    Adicionado CartaoCategoria

---
+++ Repository/Repository/CartaoRepository.cs
@@ -0,0 +1,44 @@
+﻿using Repository.DataBase;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repository
+{
+    public class CartaoRepository : ICartaoRepository
+    {
+        private SistemaContext context;
+        public CartaoRepository()
+        {
+            context = new SistemaContext();
+        }
+
+        public bool Alterar(Model.CartoesCredito cartoes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Apagar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Inserir(Model.CartoesCredito cartoes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Model.CartoesCredito ObterPeloId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Model.CartoesCredito> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
