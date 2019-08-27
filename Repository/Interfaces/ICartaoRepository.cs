ee96318e37f1037b3e83124b27f86db32aac12ea
maxjulianog <maxjulianog@gmail.com>
2019-08-27 14:29:26 -0300

    Criado Cartao Interface

---
+++ Repository/Interfaces/ICartaoRepository.cs
@@ -0,0 +1,22 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    interface ICartaoRepository
+    {
+        int Inserir(CartoesCredito cartoes);
+
+        bool Alterar(CartoesCredito cartoes);
+
+        List<CartoesCredito> ObterTodos();
+
+        bool Apagar(int id);
+
+        CartoesCredito ObterPeloId(int id);
+    }
+}
