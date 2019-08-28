01f258c504b4fabae6e5b6cbbf30fc9edd0211bf
DavidMattSchwambach <davidschwambach@hotmail.com>
2019-08-28 17:02:34 -0300

    Criado TipoController

---
+++ View/Controllers/TipoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class TipoController : Controller
+    {
+        // GET: Tipo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
