e45cc2f6d2ca30a8e0b800852c8c9cee45237e9e
DavidMattSchwambach <davidschwambach@hotmail.com>
2019-08-29 15:28:10 -0300

    Implementado BebidaController.

--- View/Controllers/BebidaController.cs
+++ View/Controllers/BebidaController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model;
+using Repository.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,58 @@ namespace View.Controllers
 {
     public class BebidaController : Controller
     {
+
+        private BebidaRepository repository;
+
         // GET: Bebida
+
+        public BebidaController()
+        {
+            repository = new BebidaRepository();
+        }
+
         public ActionResult Index()
         {
+            List<Bebida> bebidas = repository.ObterTodos();
+            ViewBag.Bebidas = bebidas;
             return View();
         }
+
+        public ActionResult Cadastro()
+        {
+            BebidaRepository bebidaRepository = new BebidaRepository();
+            List<Bebida> bebidas = bebidaRepository.ObterTodos();
+            ViewBag.Bebidas = bebidas;
+            return View();
+        }
+
+        public ActionResult Store(int idTipo, string nome, decimal valor)
+        {
+            Bebida bebida = new Bebida();
+            bebida.IdTipos = idTipo;
+            bebida.Nome = nome;
+            bebida.Valor = valor;
+            repository.Inserir(bebida);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Apagar(int id)
+        {
+            repository.Apagar(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int id)
+        {
+            Bebida bebida = repository.ObterPeloId(id);
+            ViewBag.Bebida = bebida;
+
+            BebidaRepository bebidaRepository = new BebidaRepository();
+            List<Bebida> bebidas = bebidaRepository.ObterTodos();
+            ViewBag.Bebidas = bebidas;
+            return View();
+        }
+
+        
     }
 }
