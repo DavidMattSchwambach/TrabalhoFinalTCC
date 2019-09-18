c758449bf5746ea959d646966d10ed8aba3c3dd7
evYasmin <49412927+evYasmin@users.noreply.github.com>
2019-09-18 14:38:00 -0300

    Modificado model Cartão

--- Model/Cartao.cs
+++ Model/Cartao.cs
@@ -20,10 +20,5 @@ namespace Model
         [Column("cvv")]
         public decimal CVV { get; set; }
 
-        [Column("id_cartoes_credito")]
-        public int IdCartaoCredito { get; set; }
-
-        [ForeignKey("IdCartaoCredito")]
-        public CartoesCredito CartaoCredito { get; set; }
     }
 }
