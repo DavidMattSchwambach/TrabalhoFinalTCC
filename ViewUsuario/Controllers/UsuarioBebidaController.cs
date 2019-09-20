4d5251891d38371725b55421bf1f08a50b0c4b31
evYasmin <49412927+evYasmin@users.noreply.github.com>
2019-09-20 16:24:04 -0300

    Feito Controller UsuarioBebidas. Alguns ajustes na view do usuarioBebida

---
+++ ViewUsuario/Controllers/UsuarioBebidaController.cs
@@ -0,0 +1,86 @@
+﻿using Model;
+using Repository.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewUsuario.Controllers
+{
+    public class UsuarioBebidaController : Controller
+    {
+        private BebidaRepository repository;
+
+        // GET: UsuarioBebida
+
+        public UsuarioBebidaController()
+        {
+            repository = new BebidaRepository();
+        }
+
+
+        [HttpGet, Route("Index")]
+        public ActionResult Index()
+        {
+            List<Bebida> bebidas = repository.ObterTodos();
+            ViewBag.Bebidas = bebidas;
+            return View();
+        }
+
+        [HttpGet, Route("cadastro")]
+        public ActionResult Cadastro()
+        {
+            MarcaRepository marcaRepository = new MarcaRepository();
+            ViewBag.Marcas = marcaRepository.ObterTodos();
+
+            TipoRepository tipoRepository = new TipoRepository();
+            ViewBag.Tipos = tipoRepository.ObterTodos();
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastro(Bebida bebida)
+        {
+            repository.Inserir(bebida);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet, Route("apagar")]
+        public ActionResult Apagar(int id)
+        {
+            repository.Apagar(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet, Route("editar")]
+        public ActionResult Editar(int id)
+        {
+            var bebida = repository.ObterPeloId(id);
+            ViewBag.Bebida = bebida;
+
+            MarcaRepository marcaRepository = new MarcaRepository();
+            ViewBag.Marcas = marcaRepository.ObterTodos();
+
+            TipoRepository tipoRepository = new TipoRepository();
+            ViewBag.Tipos = tipoRepository.ObterTodos();
+
+            return View();
+        }
+        [HttpPost, Route("editar")]
+        public ActionResult Editar(Bebida bebida)
+        {
+
+            repository.Alterar(bebida);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost, Route("store")]
+        public ActionResult Store(Bebida bebida)
+        {
+            repository.Inserir(bebida);
+            return RedirectToAction("Index");
+        }
+    }
+}
