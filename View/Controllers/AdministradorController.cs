a6a2e9749d45945ff0dbd25f87e23fe04d66e1bc
maxjulianog <maxjulianog@gmail.com>
2019-09-20 17:27:44 -0300

    Começado o AdministradorController

--- View/Controllers/AdministradorController.cs
+++ View/Controllers/AdministradorController.cs
@@ -1,4 +1,5 @@
-﻿using Repository.Repository;
+﻿using Model;
+using Repository.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,26 @@ namespace View.Controllers
 {
     public class AdministradorController : BaseController
     {
-       
+        private AdministradorRepository repository;
+
+        public AdministradorController()
+        {
+            repository = new AdministradorRepository();
+        }
+
+        [HttpGet, Route("Index")]
+        public ActionResult Index()
+        {
+            List<Administrador> administradors = repository.ObterTodos();
+            ViewBag.Administradores = administradors;
+            return View();
+        }
+        public ActionResult Cadastro()
+        {
+            AdministradorRepository administrador = new AdministradorRepository();
+            ViewBag.Administrador = administrador.ObterTodos();
+            return View();
+        }
+
     }
 }
