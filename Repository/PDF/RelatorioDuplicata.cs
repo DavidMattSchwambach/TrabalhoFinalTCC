4eca06856dc70435b29756334767cb5f3b4a2203
maxjulianog <maxjulianog@gmail.com>
2019-09-27 16:51:37 -0300

    Feito gerador de PDF

---
+++ Repository/PDF/RelatorioDuplicata.cs
@@ -0,0 +1,72 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Model;
+using Model.PDF;
+using Repository.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.PDF
+{
+    public class RelatorioDuplicata : TNEReport
+    {
+        public RelatorioDuplicata()
+        {
+            Paisagem = false;
+        }
+
+        public override void MontaCorpoDados()
+        {
+            base.MontaCorpoDados();
+
+            #region Cabeçalho do Relatório
+            PdfPTable table = new PdfPTable(5);
+            BaseColor preto = new BaseColor(0, 0, 0);
+            BaseColor fundo = new BaseColor(200, 200, 200);
+            Font font = FontFactory.GetFont("Verdana", 8, Font.NORMAL, preto);
+            Font titulo = FontFactory.GetFont("Verdana", 8, Font.BOLD, preto);
+
+            float[] colsW = { 10, 10, 10, 10, 10 };
+            table.SetWidths(colsW);
+            table.HeaderRows = 1;
+            table.WidthPercentage = 100f;
+
+            table.DefaultCell.Border = PdfPCell.BOTTOM_BORDER;
+            table.DefaultCell.BorderColor = preto;
+            table.DefaultCell.BorderColorBottom = new BaseColor(255, 255, 255);
+            table.DefaultCell.Padding = 10;
+
+            table.AddCell(getNewCell("Número", titulo, Element.ALIGN_LEFT, 10, PdfPCell.BOTTOM_BORDER, preto, fundo));
+            table.AddCell(getNewCell("Emissão", titulo, Element.ALIGN_LEFT, 10, PdfPCell.BOTTOM_BORDER, preto, fundo));
+            table.AddCell(getNewCell("Vencimento", titulo, Element.ALIGN_LEFT, 10, PdfPCell.BOTTOM_BORDER, preto, fundo));
+            table.AddCell(getNewCell("Valor", titulo, Element.ALIGN_RIGHT, 10, PdfPCell.BOTTOM_BORDER, preto, fundo));
+            table.AddCell(getNewCell("Saldo", titulo, Element.ALIGN_RIGHT, 10, PdfPCell.BOTTOM_BORDER, preto, fundo));
+            #endregion
+
+            var duplicatas = new DadosRelatorio().duplicatas; 
+            var clienteOld = string.Empty;
+
+            foreach (var d in duplicatas)
+            {
+                if (d.cliente.Nome != clienteOld)
+                {
+                    var cell = getNewCell(d.cliente.Nome, titulo, Element.ALIGN_LEFT, 10, PdfPCell.BOTTOM_BORDER);
+                    cell.Colspan = 5;
+                    table.AddCell(cell);
+                    clienteOld = d.cliente.Nome;
+                }
+
+                table.AddCell(getNewCell(d.Numero, font, Element.ALIGN_LEFT, 5, PdfPCell.BOTTOM_BORDER));
+                table.AddCell(getNewCell(d.Emissao.ToString("dd/MM/yyyy"), font, Element.ALIGN_LEFT, 5, PdfPCell.BOTTOM_BORDER));
+                table.AddCell(getNewCell(d.Vencimento.ToString("dd/MM/yyyy"), font, Element.ALIGN_LEFT, 5, PdfPCell.BOTTOM_BORDER));
+                table.AddCell(getNewCell(string.Format("{0:0.00}", d.Valor), font, Element.ALIGN_RIGHT, 5, PdfPCell.BOTTOM_BORDER));
+                table.AddCell(getNewCell(string.Format("{0:0.00}", d.Saldo), font, Element.ALIGN_RIGHT, 5, PdfPCell.BOTTOM_BORDER));
+            }
+
+            doc.Add(table);
+        }
+    }
+}
