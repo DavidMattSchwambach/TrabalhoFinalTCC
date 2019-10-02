31087542a6cf035d16e2b1faede9f1641f3c4dbc
maxjulianog <maxjulianog@gmail.com>
2019-10-02 13:20:05 -0300

    Retirado erro no compra Index

--- Excel/Program.cs
+++ Excel/Program.cs
@@ -44,7 +44,7 @@ namespace Excel
 
             ws.Columns("2 - 9").AdjustToContents();
 
-            wb.SaveAs("C:/Users/CNBarbaNegra/Desktop/Excel/teste_tne.xlsx");
+            wb.SaveAs("C:/Users/65978/Desktop/Excel/teste_tne.xlsx");
 
             wb.Dispose();
 
